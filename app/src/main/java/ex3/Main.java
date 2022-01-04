8cd56cefffbebb459e229a410bc198784adda15a
Taisei Maetake <e215703@ie.u-ryukyu.ac.jp>
2022-01-04 13:21:33 +0900

    実験

--- app/src/main/java/ex3/Main.java
+++ app/src/main/java/ex3/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args){
         Dice dice = new Dice();//エラー出たので一旦終了
         System.out.println(dice.getValue());
+        System.out.println("実験");
     }
 }
